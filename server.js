96037a174bce24086c7a3dee14a0a2f9cbd3d056
Trevor Bos <trevor.bos@outlook.com>
2022-07-11 20:09:13 -0400

    Not sure why its asking me to save, I thought I already did.

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 // Grabbing the express stuff to setup the server
 const express = require('express');
 
-
 // app variable for using express
 const app = express();
-
 
 //create the port
 const port = process.env.PORT || 3001;
 
-
 //create the routes for files in the public folder
-
+app.use(express.static('public'));
 
 //Handle data parsing, middleware and req.body
-
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 //Route to the files
 
