084ce1462034e6c149cf5ea8ce10aeb277d38071
Trevor Bos <trevor.bos@outlook.com>
2022-07-11 20:21:05 -0400

    Added some basic comments for the api routing sheet.

---
+++ routes/apiRoute.js
@@ -0,0 +1,11 @@
+// Initial variables
+
+// Going to use an npm package that will allow different IDs to be created for each note that is added.
+
+// Routing for the application
+
+// get request - should read the db file and return all the notes within as a JSON
+
+// POST request - should receive the new note (req.body) and add it to the JSON, then return note to display.
+
+// Bonus DELETE request - should receive the id so that it can be removed.
