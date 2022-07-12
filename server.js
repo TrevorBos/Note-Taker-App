2755a12619db6d34e2bc8946d517a668420511ab
Trevor Bos <trevor.bos@outlook.com>
2022-07-11 20:03:58 -0400

    started adding the information into the server file.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // Grabbing the express stuff to setup the server
+const express = require('express');
 
 
 // app variable for using express
+const app = express();
 
 
 //create the port
+const port = process.env.PORT || 3001;
 
 
 //create the routes for files in the public folder
