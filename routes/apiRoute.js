6731db4fcb73047dd96a02c40dbab7f181df879c
Trevor Bos <trevor.bos@outlook.com>
2022-07-11 20:39:40 -0400

    Potential bonus deletion feature added.

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -17,21 +17,30 @@ module.exports = (app) => {
 };
 
 // POST request - should receive the new note (req.body) and add it to the JSON, then return note to display.
-app.post('/api/notes', (req, res) => {
-    let database = fs.readFileSync('db/db.json');
-    database = JSON.parse(database);
-    res.JSON(database);
-    // This should create the body for the note
-    let myNote = {
-        title: req.body.title,
-        text: req.body.text,
-        //this is where we will use the npm package uniqid to set a fancy id for each note and hopefully allow for deletion
-        id: uniqid(),
-    };
-    // We then want to push the information to the db.json file
-    database.push(myNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(database));
-    res.json(database);
+app.post("/api/notes", (req, res) => {
+  let database = fs.readFileSync("db/db.json");
+  database = JSON.parse(database);
+  res.JSON(database);
+  // This should create the body for the note
+  let myNote = {
+    title: req.body.title,
+    text: req.body.text,
+    //this is where we will use the npm package uniqid to set a fancy id for each note and hopefully allow for deletion
+    id: uniqid(),
+  };
+  // We then want to push the information to the db.json file
+  database.push(myNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(database));
+  res.json(database);
 });
 
 // Bonus DELETE request - should receive the id so that it can be removed.
+app.delete("/api/notes", (req, res) => {
+  // read the notes
+  let database = JSON.parse(fs.readFileSync("db/db.json"));
+  // remove the notes via the unique ID given
+  let noteDelete = database.filter((item) => item.id !== req.params.id);
+  // Rewrite the file
+  fs.writeFileSync("db/db.json", JSON.stringify(noteDelete));
+  res.json(noteDelete);
+});
