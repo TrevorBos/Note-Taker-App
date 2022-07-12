a1768a87d16677b300af63ddd0f3d659ad3fe8ce
Trevor Bos <trevor.bos@outlook.com>
2022-07-11 20:00:56 -0400

    Added some basic comments to guide the process

---
+++ server.js
@@ -0,0 +1,19 @@
+// Grabbing the express stuff to setup the server
+
+
+// app variable for using express
+
+
+//create the port
+
+
+//create the routes for files in the public folder
+
+
+//Handle data parsing, middleware and req.body
+
+
+//Route to the files
+
+
+//app listener (this is how the server should start)
